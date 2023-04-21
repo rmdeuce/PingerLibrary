c1c2e3e6562c1f62cd22b5da9bbabde6fe320193
rmdeuce <rmdeuce@gmail.com>
2023-04-21 17:39:05 +1000

    add list with dictionary blocks servers

--- Pinganator/Service/Classes/PingWorker.cs
+++ Pinganator/Service/Classes/PingWorker.cs
@@ -18,53 +18,58 @@ namespace Pinganator.Service.Classes
         public void Polling()
         {
             int countServers = Servers.Count;
-            
+
             Result = new ConcurrentDictionary<int, PingReply>();
 
-            Task[] arrayTasks = new Task[countServers];
-            //Таймер
-            Stopwatch stopwatch = new Stopwatch();
+            //Task[] arrayTasks = new Task[countServers];
+
+            Stopwatch stopwatch = new Stopwatch(); //Таймер
             stopwatch.Start();
-            //
+
             if (countServers == 0)
                 return;
-            // Создание разбиения коллекции серверов с размером части 10
-            var partitioner = Partitioner.Create(0, countServers, 10);
             
-            Parallel.ForEach(partitioner, (part) =>
-            {
-                //Получение индекса начала и конца части
-                int start = part.Item1;
-                int end = part.Item2;
-                Ping pinger = new Ping();
-                //Выполнение Task в части
-                for (int i = start; i < end; i++)
-                {                     
-                    try
-                    {
-                        var server = Servers.ElementAt(i);
-                        Result[server.Key] = pinger.Send(server.Value);
-                        //Console.WriteLine(Result[server.Key].Address);
-                    }
-                    catch (Exception ex)
-                    { }
-                }
-            });
+            int blockSize = Servers.Count / 10;
 
-            //Остановка таймера
-            stopwatch.Stop(); 
+            var arr = new List<Dictionary<int, IPAddress>>();
+
+            arr.Add(new Dictionary<int, IPAddress>(blockSize));
+
+            var current = arr[0];
+
+            for (int i = 0; i < Servers.Count; i++)
+            {
+
+                if (i != 0 && i % blockSize == 0)
+                {
+                    var curDic = new Dictionary<int, IPAddress>(blockSize);
+
+                    arr.Add(curDic);
+
+                    current = curDic;
+
+                    current.Add(Servers.ElementAt(i).Key, Servers.ElementAt(i).Value);
+                }
+                else
+                {
+                    current.Add(Servers.ElementAt(i).Key, Servers.ElementAt(i).Value);
+                }
+
+            }
+
+            stopwatch.Stop(); //Остановка таймера
             TimeSpan ts = stopwatch.Elapsed;
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
             Console.WriteLine("RunTime " + elapsedTime); 
         }
 
-        private void ParallelPing(Dictionary<int, IPAddress> Servers, ConcurrentDictionary<int, PingReply> Result)
+        private void ParallelPing(Dictionary<int, IPAddress> inputDict, ConcurrentDictionary<int, PingReply> outputDict)
         {
-            int countServers = Servers.Count;
+            int countServers = inputDict.Count;
             if (countServers == 0)
                 return;
 
-            Parallel.ForEach(Servers, (server) =>
+            Parallel.ForEach(inputDict, (server) =>
             {
                 Ping pinger = new Ping();
 
