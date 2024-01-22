03a5031256f3b46a83f953d73fec6722eacacfdf
rmdeuce <rmdeuce@gmail.com>
2024-01-22 17:26:07 +1000

    Добавлены изменения

--- ConsoleApp/obj/Debug/net7.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net7.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8dbd0cedefab41971beed948fd393ae53364e27")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
